ecbbc263b8247320d0ac0d00694ef0f8fbd618bb
Will Fox <fox.wi@northeastern.edu>
2025-03-26 21:44:51 -0400

    removed un-needed comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 // YourArduinoSketch.ino
 
+// include your class header files here
 #include "LEDBlinker.h"
 
 LEDBlinker myLED(25); // Change 13 to the pin your LED is connected to
-LEDBlinker myLED2(26);
+LEDBlinker myLED2(26); // If you wanted to add a second instance of the class
 
 void setup() {
   // No setup needed for this example
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -14,63 +16,3 @@ void loop() {
 
   // need to add something else
 }
-
-
-
-
-
-
-
-
-
-
-
-// #include <Arduino.h>
-// #include "ExClass.h" // Include your ExClass header
-
-// // Create an instance of the ExClass
-// // ExClass myDevice;
-
-// // Pin number for the LED
-// // const int ledPin = 12;
-// uint8_t ledPin = 25;
-
-// void setup() {
-//   Serial.begin(9600);
-
-//   ExClass myDevice;
-
-//   // Set up pins in the ExClass object
-//   // myDevice.setControlPins(2, 3); // Example motor pins
-//   // myDevice.setInputPin(4); // Example input pin
-//   myDevice.setLedPin(ledPin); // Set the LED pin
-//   // pinMode(ledPin, OUTPUT); // Set the LED pin as output
-// }
-
-// void loop() {
-//   // Turn LED on and off
-//   // myDevice.turnLedOn();
-//   // delay(1000);
-//   // Serial.println("LED on");
-//   // myDevice.turnLedOff();
-//   // delay(1000);
-//   // Serial.println("LED off");
-//   digitalWrite(ledPin, HIGH);
-//   delay(1000);
-//   Serial.println("LED on");
-//   digitalWrite(ledPin, LOW);
-//   delay(1000);
-//   Serial.println("LED off");
-
-//   // Example: Read an input and control a motor
-//   // bool inputState = myDevice.readInput();
-//   // if (inputState) {
-//   //   myDevice.startMotor(true); // Start motor forward
-//   //   Serial.println("Motor forward.");
-//   // } else {
-//   //   myDevice.startMotor(false); // Start motor reverse
-//   //   Serial.println("Motor reverse.");
-//   // }
-
-//   // delay(100); // Small delay to prevent rapid input reading
-// }
