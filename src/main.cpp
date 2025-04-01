123ee9b0dda52966f3f93c96c7156b38bf93bf72
Nik Varga <ndv2003.ndv@gmail.com>
2025-03-31 20:47:40 -0400

    START STATE MACHINE

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,14 @@
 
 // include your class header files here
 #include "LEDBlinker.h"
-
-LEDBlinker myLED(25); // Change 13 to the pin your LED is connected to
-LEDBlinker myLED2(26); // If you wanted to add a second instance of the class
+#include <Adafruit_MCP23X17.h>
 
 void setup() {
   // No setup needed for this example
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
-  myLED.blink(500); // Blink every 500 milliseconds
 
   // need to add something else
 }
---
+++ src/stateMachine.hpp
@@ -0,0 +1,21 @@
+#ifndef STATEMACHINE
+#define STATEMACHINE_HPP
+
+enum STATES {
+    STANDBY,
+    HEATING,
+    DISPENSING,
+    MIXING,
+    WICK_PLACEMENT,
+    COOLING,
+    EJECTING
+};
+
+class StateMachine {
+    public:
+
+    private:
+
+};
+
+#endif
